304af9360d46c7cc7f4ae0ed0f35b7469e668201
Lucas H B Medeiros <120293165+Lucaasshq@users.noreply.github.com>
2025-08-22 22:31:48 -0300

    criando pagina de registrar-se, no front end

--- gerenciador-tarefas-front-end/src/app/auth/components/signup/signup.component.ts
+++ gerenciador-tarefas-front-end/src/app/auth/components/signup/signup.component.ts
@@ -1,11 +1,35 @@
+import { reqHandler } from './../../../../server';
 import { Component } from '@angular/core';
+import { MaterialModule } from "../../../MaterialModule";
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [MaterialModule,CommonModule, ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+
+    signupForm!: FormGroup;
+    mostrarSenha = false;
+
+    constructor(private fb: FormBuilder){
+      this.signupForm = this.fb.group({
+        nome:[null,[Validators.required]],
+        email:[null,[Validators.required, Validators.email]],
+        password:[null,[Validators.required]],
+        confirmarPassword:[null,[Validators.required]]
+      })
+    }
+
+    alternarVisibilidadePassword(){
+      this.mostrarSenha = !this.mostrarSenha
+    }
+
+    enviarAgora() {
+      console.log("enviado")
+}
 
 }
