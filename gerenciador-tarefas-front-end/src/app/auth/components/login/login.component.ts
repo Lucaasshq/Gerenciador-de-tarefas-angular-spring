819af59ec57fe018ce801b2cc38af59d7318e931
Lucas H B Medeiros <120293165+Lucaasshq@users.noreply.github.com>
2025-08-24 13:48:37 -0300

    feat: criando pagina de login

--- gerenciador-tarefas-front-end/src/app/auth/components/login/login.component.ts
+++ gerenciador-tarefas-front-end/src/app/auth/components/login/login.component.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../../../MaterialModule';
-import { FormGroup, FormsModule } from '@angular/forms';
+import { Form, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [MaterialModule, FormsModule, CommonModule],
+  imports: [MaterialModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  mostrarSenha: boolean = true;;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]]
+    })
+  }
 
+  alterarVisibilidadePassword(): void {
+    this.mostrarSenha = !this.mostrarSenha
+  }
+
+  enviarAgora(): void {
+    console.log("Login feito")
+  }
 
 }
--- gerenciador-tarefas-front-end/src/app/auth/components/signup/signup.component.ts
+++ gerenciador-tarefas-front-end/src/app/auth/components/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { reqHandler } from './../../../../server';
+
 import { Component } from '@angular/core';
 import { MaterialModule } from "../../../MaterialModule";
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
@@ -6,30 +6,30 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
-  imports: [MaterialModule,CommonModule, ReactiveFormsModule],
+  imports: [MaterialModule, CommonModule, ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
 
-    signupForm!: FormGroup;
-    mostrarSenha = false;
+  signupForm!: FormGroup;
+  mostrarSenha = true;
 
-    constructor(private fb: FormBuilder){
-      this.signupForm = this.fb.group({
-        nome:[null,[Validators.required]],
-        email:[null,[Validators.required, Validators.email]],
-        password:[null,[Validators.required]],
-        confirmarPassword:[null,[Validators.required]]
-      })
-    }
+  constructor(private fb: FormBuilder) {
+    this.signupForm = this.fb.group({
+      nome: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]],
+      confirmarPassword: [null, [Validators.required]]
+    })
+  }
 
-    alternarVisibilidadePassword(){
-      this.mostrarSenha = !this.mostrarSenha
-    }
+  alternarVisibilidadePassword() {
+    this.mostrarSenha = !this.mostrarSenha
+  }
 
-    enviarAgora() {
-      console.log("enviado")
-}
+  enviarAgora() {
+    console.log("enviado")
+  }
 
 }
